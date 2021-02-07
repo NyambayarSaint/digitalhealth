81a170d61ede3f6400852a6f73a1884794e8cf3f
Nyambayar S <web-master@tavanbogd.com>
2021-02-07 16:30:09 +0800

    save

--- components/Image.js
+++ components/Image.js
@@ -4,18 +4,8 @@ import minimize from './miscs/minimize';
 
 const Image = ({data}) => {
 
-    const img = React.useRef();
-    const [height, setHeight] = React.useState(0);
-    console.log(height,'yey')
-    React.useEffect(()=>{
-        setTimeout(() => {
-            setHeight(img.current.offsetHeight);
-        }, 1000);
-    },[]);
-
     return (
-        <Container height={height}>
-            <img ref={img} src={minimize(data.Image)}/>
+        <Container>
             <div className="image" style={{backgroundImage: `url(${minimize(data.Image)})`}}></div>
         </Container>
     );
@@ -24,26 +14,19 @@ const Image = ({data}) => {
 export default Image;
 
 const Container = styled.div `
-    ${({height}) => height && `
-    img{
-        display:none;
-    }
-    `};
     .image{
         width:100%;
-        height: ${({height}) => height && height + 'px'};
+        height: 700px;
         background-size: cover;
         background-repeat:no-repeat;
         background-attachment: fixed;
         background-position: center;
     }
     @media only screen and (max-width: 768px){
-        height:auto !important;
-        img{
-            display:block;
-        }
         .image{
-            display:none;
+            height:200px;
+            background-size: contain;
+            background-attachment: unset;
         }
     }
 `
